877a112cec49c3d78b5ca5a4effd578fc0474316
Bill McCord <mccord.bill@gmail.com>
2017-01-26 13:51:11 +0800

    Changed salary to a string field. Added a description field of medium text size.

--- app/modules/jobs/models.py
+++ app/modules/jobs/models.py
@@ -6,6 +6,7 @@
     Jobs module models
 """
 import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
 
 from app.extensions import db
 from app.modules.base import Base
@@ -29,7 +30,8 @@ class Job(Base):
     hiring_company = db.Column(db.String(255))
     location = db.Column(db.String(255))
     work_type = db.Column(db.String(255))
-    expected_salary = db.Column(db.BigInteger)  # HKD annual?
+    expected_salary = db.Column(db.String(255))
+    description = db.Column(mysql.MEDIUMTEXT)
     is_published = db.Column(
         db.Boolean,
         server_default=sa.text("'0'"),
