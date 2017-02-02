22f20f8751e5d527749f0f8328fd69e1926706b9
AlexAnzolaJ <alex.anzolaj@gmail.com>
2017-02-02 17:27:34 +0800

    Unpublished jobs don't accept CV. Don't show Download button if candidate hasn't uploaded a resume

--- app/modules/admin/__init__.py
+++ app/modules/admin/__init__.py
@@ -13,8 +13,7 @@ from .views import AppAdminIndexView, UserModelView, RoleModelView, \
 
 
 def init_admin(app):
-    admin = Admin(app, name=u'Admin', template_mode='bootstrap3',
-                  index_view=AppAdminIndexView())
+    admin = Admin(app, name=u'Admin', template_mode='bootstrap3')
     admin.add_view(UserModelView())
     admin.add_view(RoleModelView())
     admin.add_view(CompanyModelView())
